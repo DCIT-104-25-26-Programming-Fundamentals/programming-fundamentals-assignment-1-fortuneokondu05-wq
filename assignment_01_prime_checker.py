ad85721540da15e666667275a95e1e17df699de2
Fortune Okondu <fortuneokondu05@gmail.com>
2026-07-26 17:42:32 +0000

    Complete assignment 1: prime checker

--- assignment_01_prime_checker.py
+++ assignment_01_prime_checker.py
@@ -34,4 +34,15 @@
 # =============================================================================
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
-
+def is_prime(n):
+    if n<2:
+        return False
+    for i in range(2,n):
+        if n%i ==0:
+            return False
+    return True
+num = int(input("Enter a number: "))
+if is_prime(num) :
+    print(f"{num} is a prime number.")
+else:
+    print(f"{num} is NOT a prime number. ")       
